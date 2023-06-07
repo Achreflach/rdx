09a9e5b9825e59ec198957c09d8be921d9ad5f93
achrefbz <achrefbouzgarrou@yahoo.com>
2023-06-07 14:18:05 +0100

    firsttt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { Form } from "./components/Form";
+import { Todos } from "./components/Todos";
+import {useDispatch, useSelector} from 'react-redux';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+ 
+  const dispatch = useDispatch();
+
+  const todos = useSelector((state)=>state.operationsReducer);
+
+ 
+  const [editFormVisibility, setEditFormVisibility]=useState(false);
+
+ 
+  const [editTodo, setEditTodo]=useState('');
+
+  
+  const handleEditClick=(todo)=>{
+    setEditFormVisibility(true);
+    setEditTodo(todo);
+  }
+
+  const cancelUpdate=()=>{
+    setEditFormVisibility(false);
+  }
+
+    return (
+    <div className="wrapper">
+      <br></br>
+      <h1 className="text-center">TODO-APP</h1>
+      <Form editFormVisibility={editFormVisibility} editTodo={editTodo}
+      cancelUpdate={cancelUpdate}/>
+      <Todos handleEditClick={handleEditClick} editFormVisibility={editFormVisibility}/>
+      {todos.length > 1}
     </div>
   );
 }
